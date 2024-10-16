ab4453bf6ea42904ca884bede43cd631bee4d5a8
Dau Trong Hoang <45507664+sakkarose@users.noreply.github.com>
2024-10-16 22:52:38 +0700

    Update lastfm-recent-tracks.js

--- static/js/lastfm-recent-tracks.js
+++ static/js/lastfm-recent-tracks.js
@@ -6,28 +6,36 @@ fetch('/lastfm-proxy')
     return response.json();
   })
   .then(data => {
-    console.log("Data received:", data); 
+    console.log("Data received:", data);
 
-    // Access properties directly from the data object
-    const artist = data.artist;
-    const songName = data.name;
-    const albumName = data.album;
-    const artworkUrl = data.artworkUrl;
-    const songUrl = data.songUrl;
-    const nowPlaying = "Recently played";
+    // Check if data and required properties exist
+    if (data && data.artist && data.name && data.album && data.artworkUrl && data.songUrl) { 
+      const artist = data.artist;
+      const songName = data.name;
+      const albumName = data.album;
+      const artworkUrl = data.artworkUrl;
+      const songUrl = data.songUrl;
+      const nowPlaying = "Recently played";
 
-    const trackContainer = document.getElementById('lastfm-recent-track');
-    trackContainer.innerHTML = `
-      <div><h2><i class="fab fa-lastfm"></i> <span class="now-playing">${nowPlaying}</span></h2>
-        <div style="display: flex; align-items: left;">
-          <img src="${artworkUrl}" alt="Album artwork for ${albumName}" onerror="this.src='path/to/default-image.jpg';">
-          <div class="track-info">
-            <h3 class="track-title"><span><a href="${songUrl}" target="_blank">${songName}</a></span></h3>
-            <p class="artist">${artist}</p> <p class="album">${albumName}</p> 
+      const trackContainer = document.getElementById('lastfm-recent-track');
+      trackContainer.innerHTML = ` 
+        <div>
+          <h2><i class="fab fa-lastfm"></i> <span class="now-playing">${nowPlaying}</span></h2>
+          <div style="display: flex; align-items: left;">
+            <img src="${artworkUrl}" alt="Album artwork for ${albumName}" onerror="this.src='path/to/default-image.jpg';">
+            <div class="track-info">
+              <h3 class="track-title"><span><a href="${songUrl}" target="_blank">${songName}</a></span></h3>
+              <p class="artist">${artist}</p>
+              <p class="album">${albumName}</p> 
+            </div>
           </div>
         </div>
-      </div>
-    `;
+      `;
+    } else {
+      // Handle the case where the expected data is not found
+      console.error("Last.fm data is missing expected properties:", data);
+      document.getElementById('lastfm-recent-track').innerHTML = '<p>Unable to load your recent track. Please try again later.</p>';
+    }
   })
   .catch(err => {
     console.error("Error fetching or parsing Last.fm data:", err);
