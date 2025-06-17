3bbc8ee7db55765e73f1637224281310c10fc086
Dau Trong Hoang <ame@mizu.reisen>
2025-06-18 00:53:47 +0700

    Update lastfm-recent-tracks.js

--- static/js/lastfm-recent-tracks.js
+++ static/js/lastfm-recent-tracks.js
@@ -6,7 +6,9 @@ fetch('https://lastfmapi.last-continue103.workers.dev')
     return response.json();
   })
   .then(data => {
-    console.log("Data received:", data);    // Check if data and required properties exist    if (data?.artist && data?.name && data?.album && data?.artworkUrl && data?.songUrl) { 
+    console.log("Data received:", data);
+    // Check if data and required properties exist
+    if (data?.artist && data?.name && data?.album && data?.artworkUrl && data?.songUrl) { 
       const { artist, name: songName, album: albumName, artworkUrl, songUrl } = data;
 
       const trackContainer = document.getElementById('lastfm-recent-track');
@@ -14,7 +16,7 @@ fetch('https://lastfmapi.last-continue103.workers.dev')
         <div>
           <h2><i class="fab fa-lastfm"></i> <span class="now-playing">Recently Played</span></h2> 
           <div style="display: flex; align-items: left;">
-            <img src="${artworkUrl}" alt="Album artwork for ${albumName}" onerror="this.src='path/to/default-image.jpg';">
+            <img src="${artworkUrl}" alt="Album artwork for ${albumName}" onerror="this.src='/images/default-album.webp';">
             <div class="track-info">
               <h3 class="track-title"><span><a href="${songUrl}" target="_blank">${songName}</a></span></h3>
               <p class="artist">${artist}</p>
@@ -30,5 +32,5 @@ fetch('https://lastfmapi.last-continue103.workers.dev')
   })
   .catch(err => {
     console.error("Error fetching or parsing Last.fm data:", err);
-    document.getElementById('lastfm-recent-track').innerHTML = '<p>Unable to load your recent track. Please try again later.</p>';
+    document.getElementById('lastfm-recent-track').innerHTML = `<p>Unable to load your recent track: ${err.message}</p>`;
   });
