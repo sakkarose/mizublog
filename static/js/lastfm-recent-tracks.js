e6542fd803ca0d845cf111baa33b1fc77efa3dc9
Dau Trong Hoang <ame@mizu.reisen>
2025-06-18 00:22:53 +0700

    Caching lastfm

--- static/js/lastfm-recent-tracks.js
+++ static/js/lastfm-recent-tracks.js
@@ -1,4 +1,4 @@
-fetch('/lastfm-proxy')
+fetch('/js/lastfm-data.json')
   .then(response => {
     if (!response.ok) {
       throw new Error(`Network response was not ok (status: ${response.status})`);
